208f6c6624712dba8176effd179c0ac4d4b8dc86
Manu <emanuelnazare@yahoo.ro>
2017-09-20 13:41:57 +0300

    - added google analytics

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createBrowserHistory } from 'history';
-
 import {
     HashRouter,
     Route,
     Switch
 } from 'react-router-dom';
+import ReactGA from 'react-ga';
+
 
 import App from 'containers/App/App.jsx';
 import Components from 'containers/Components/Components.jsx';
@@ -19,9 +20,15 @@ import 'assets/css/pe-icon-7-stroke.css';
 import 'containers/Components/assets/css/Components.css';
 
 const history = createBrowserHistory();
+ReactGA.initialize('UA-000000-01');
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname + window.location.search });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 ReactDOM.render((
-    <HashRouter history={history}>
+    <HashRouter history={history} onUpdate={logPageView}>
         <Switch>
             <Route path="/components" name="Components" component={Components}/>
             <Route path="/" name="Home" component={App}/>
