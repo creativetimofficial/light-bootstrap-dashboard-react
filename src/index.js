51f0bc99243e3ce4c7f54df6e686ce8153553f92
Gordon Sun <sunshi_neo@hotmail.com>
2019-01-27 12:20:06 -0800

    Use BrowserRouter to remove # from path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import indexRoutes from "routes/index.jsx";
 
@@ -12,12 +12,12 @@ import "./assets/css/demo.css";
 import "./assets/css/pe-icon-7-stroke.css";
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <Switch>
       {indexRoutes.map((prop, key) => {
         return <Route to={prop.path} component={prop.component} key={key} />;
       })}
     </Switch>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
