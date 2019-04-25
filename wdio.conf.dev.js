68d9447caf39e2cf2df5cd1c2d45666b3816e20e
carmenmitru <carmen.mitru@gmail.com>
2019-04-25 08:17:56 +0300

    add testing

---
+++ wdio.conf.dev.js
@@ -0,0 +1,86 @@
+let path = require("path");
+let notifier = require("node-notifier");
+
+let VisualRegressionCompare = require("wdio-visual-regression-service/compare");
+function getScreenshotName(folder, context) {
+  const type = context.type;
+  const testParent = context.test.parent;
+  const testName = context.test.title;
+  const browserVersion = parseInt(context.browser.version, 10);
+  const browserName = context.browser.name;
+  const browserViewport = context.meta.viewport;
+  const browserWidth = browserViewport.width;
+  const browserHeight = browserViewport.height;
+
+  return path.join(
+    process.cwd(),
+    folder,
+    `${testParent}_${testName}_${type}_${browserName}_v${browserVersion}_${browserWidth}x${browserHeight}.png`
+  );
+}
+
+/** Prod Config File */
+let prodConfig = require("./wdio.conf.js").config;
+
+let localConfig = Object.assign(prodConfig, {
+  baseUrl: "http://localhost:3000",
+  specs: ["./tests/*.spec.js"],
+  capabilities: [
+    {
+      browserName: "chrome",
+      maxInstances: 1,
+      loggingPrefs: {
+        driver: "ALL",
+        browser: "ALL"
+      }
+    }
+  ],
+
+  services: ["selenium-standalone", "visual-regression", "static-server"],
+
+  visualRegression: {
+    compare: new VisualRegressionCompare.LocalCompare({
+      referenceName: getScreenshotName.bind(null, "screenshots/baseline"),
+      screenshotName: getScreenshotName.bind(null, "screenshots/latest"),
+      diffName: getScreenshotName.bind(null, "screenshots/diff")
+    }),
+    viewportChangePause: 300,
+    viewports: [
+      { width: 1280, height: 720 }
+    ],
+    orientations: ["landscape", "portrait"]
+  },
+
+  reporters: ["concise", "slack"],
+  seleniumInstallArgs: { version: "3.4.0" },
+  seleniumArgs: { version: "3.4.0" },
+  reporterOptions: {
+    slack: {
+      notify: true,
+      webhook: process.env.SLACK_WEBHOOK,
+      notifyOnlyOnFailure: true,
+      username: "Running Tests Report",
+      message: "Light Bootstrap Dashboard React"
+    }
+  },
+  onPrepare: function(config, capabilities) {
+    notifier.notify({
+      title: "Light Bootstrap Dashboard React",
+      message: "Test run started"
+    });
+  },
+
+  afterTest(test) {},
+  onComplete: function(exitCode) {
+    notifier.notify({
+      title: "Light Bootstrap Dashboard React",
+      message: "Tests finished running."
+    });
+  }
+});
+
+delete localConfig.user;
+delete localConfig.key;
+delete localConfig.sauceConnect;
+
+exports.config = localConfig;
