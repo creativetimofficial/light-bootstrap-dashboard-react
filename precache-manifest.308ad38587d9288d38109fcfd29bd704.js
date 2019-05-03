5dc0d1541c5fdafb699a7e00f115925e4beae663
Manu <emanuelnazare@yahoo.ro>
2019-05-03 19:30:25 +0300

    Updates

---
+++ precache-manifest.308ad38587d9288d38109fcfd29bd704.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f828943924e3b0f01f7e7a27997cca",
+    "url": "/light-bootstrap-dashboard-react/index.html"
+  },
+  {
+    "revision": "e3883a9f76711b6bb8d7",
+    "url": "/light-bootstrap-dashboard-react/static/css/2.bce45138.chunk.css"
+  },
+  {
+    "revision": "4d7cb580c4cd09344077",
+    "url": "/light-bootstrap-dashboard-react/static/css/main.6e97503c.chunk.css"
+  },
+  {
+    "revision": "e3883a9f76711b6bb8d7",
+    "url": "/light-bootstrap-dashboard-react/static/js/2.a644ba5a.chunk.js"
+  },
+  {
+    "revision": "4d7cb580c4cd09344077",
+    "url": "/light-bootstrap-dashboard-react/static/js/main.b62af183.chunk.js"
+  },
+  {
+    "revision": "0ce3b4941c0b8964810e",
+    "url": "/light-bootstrap-dashboard-react/static/js/runtime~main.6b81f0f5.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/light-bootstrap-dashboard-react/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/light-bootstrap-dashboard-react/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/light-bootstrap-dashboard-react/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "c45f7de008ab976a8e817e3c0e5095ca",
+    "url": "/light-bootstrap-dashboard-react/static/media/Pe-icon-7-stroke.c45f7de0.svg"
+  },
+  {
+    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
+    "url": "/light-bootstrap-dashboard-react/static/media/face-3.ce7a6b79.jpg"
+  },
+  {
+    "revision": "9f2907bea36d49032d7148904fb24eec",
+    "url": "/light-bootstrap-dashboard-react/static/media/full-image-1.9f2907be.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/light-bootstrap-dashboard-react/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/light-bootstrap-dashboard-react/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/light-bootstrap-dashboard-react/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/light-bootstrap-dashboard-react/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/light-bootstrap-dashboard-react/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "9b864b3680f372ff9ae02649fbc44838",
+    "url": "/light-bootstrap-dashboard-react/static/media/reactlogo.9b864b36.png"
+  },
+  {
+    "revision": "6be21e8a1b7d63048728851c6003e189",
+    "url": "/light-bootstrap-dashboard-react/static/media/sidebar-1.6be21e8a.jpg"
+  },
+  {
+    "revision": "34535b958ad0f0183770a8731b31b4ca",
+    "url": "/light-bootstrap-dashboard-react/static/media/sidebar-3.34535b95.jpg"
+  },
+  {
+    "revision": "7bc259fd5cef15e95376c822c7e23081",
+    "url": "/light-bootstrap-dashboard-react/static/media/sidebar-4.7bc259fd.jpg"
+  },
+  {
+    "revision": "8336c4e99f1e8bddccb1aeef2fb7cea4",
+    "url": "/light-bootstrap-dashboard-react/static/media/sidebar-5.8336c4e9.jpg"
+  }
+]);
