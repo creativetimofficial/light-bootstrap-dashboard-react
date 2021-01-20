9be3a0de0f5b5ef98ce28496f44c306a3a21880d
EINazare <emanuelioannazare@gmail.com>
2021-01-20 15:36:12 +0200

    style: run prettier

--- src/index.js
+++ src/index.js
@@ -20,11 +20,11 @@ import ReactDOM from "react-dom";
 
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/animate.min.css";
 import "./assets/scss/light-bootstrap-dashboard-react.scss?v=1.3.0";
 import "./assets/css/demo.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import AdminLayout from "layouts/Admin.jsx";
 
