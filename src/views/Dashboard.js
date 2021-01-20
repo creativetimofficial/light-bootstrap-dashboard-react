bbed7e35a33d2959a290d698e2f40082be930772
EINazare <emanuelioannazare@gmail.com>
2021-01-20 18:32:16 +0200

    feature: add dashboard code

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import ChartistGraph from "react-chartist";
 // react-bootstrap components
 import {
   Badge,
@@ -127,31 +127,6 @@ function Dashboard() {
           </Col>
         </Row>
         <Row>
-          <Col md="4">
-            <Card>
-              <Card.Header>
-                <Card.Title as="h4">Email Statistics</Card.Title>
-                <p className="card-category">Last Campaign Performance</p>
-              </Card.Header>
-              <Card.Body>
-                <div
-                  className="ct-chart ct-perfect-fourth"
-                  id="chartPreferences"
-                ></div>
-                <div className="legend">
-                  <i className="fas fa-circle text-info"></i>
-                  Open <i className="fas fa-circle text-danger"></i>
-                  Bounce <i className="fas fa-circle text-warning"></i>
-                  Unsubscribe
-                </div>
-                <hr></hr>
-                <div className="stats">
-                  <i className="far fa-clock"></i>
-                  Campaign sent 2 days ago
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
           <Col md="8">
             <Card>
               <Card.Header>
@@ -159,7 +134,56 @@ function Dashboard() {
                 <p className="card-category">24 Hours performance</p>
               </Card.Header>
               <Card.Body>
-                <div className="ct-chart" id="chartHours"></div>
+                <div className="ct-chart" id="chartHours">
+                  <ChartistGraph
+                    data={{
+                      labels: [
+                        "9:00AM",
+                        "12:00AM",
+                        "3:00PM",
+                        "6:00PM",
+                        "9:00PM",
+                        "12:00PM",
+                        "3:00AM",
+                        "6:00AM",
+                      ],
+                      series: [
+                        [287, 385, 490, 492, 554, 586, 698, 695],
+                        [67, 152, 143, 240, 287, 335, 435, 437],
+                        [23, 113, 67, 108, 190, 239, 307, 308],
+                      ],
+                    }}
+                    type="Line"
+                    options={{
+                      low: 0,
+                      high: 800,
+                      showArea: false,
+                      height: "245px",
+                      axisX: {
+                        showGrid: false,
+                      },
+                      lineSmooth: true,
+                      showLine: true,
+                      showPoint: true,
+                      fullWidth: true,
+                      chartPadding: {
+                        right: 50,
+                      },
+                    }}
+                    responsiveOptions={[
+                      [
+                        "screen and (max-width: 640px)",
+                        {
+                          axisX: {
+                            labelInterpolationFnc: function (value) {
+                              return value[0];
+                            },
+                          },
+                        },
+                      ],
+                    ]}
+                  />
+                </div>
               </Card.Body>
               <Card.Footer>
                 <div className="legend">
@@ -176,6 +200,39 @@ function Dashboard() {
               </Card.Footer>
             </Card>
           </Col>
+          <Col md="4">
+            <Card>
+              <Card.Header>
+                <Card.Title as="h4">Email Statistics</Card.Title>
+                <p className="card-category">Last Campaign Performance</p>
+              </Card.Header>
+              <Card.Body>
+                <div
+                  className="ct-chart ct-perfect-fourth"
+                  id="chartPreferences"
+                >
+                  <ChartistGraph
+                    data={{
+                      labels: ["40%", "20%", "40%"],
+                      series: [40, 20, 40],
+                    }}
+                    type="Pie"
+                  />
+                </div>
+                <div className="legend">
+                  <i className="fas fa-circle text-info"></i>
+                  Open <i className="fas fa-circle text-danger"></i>
+                  Bounce <i className="fas fa-circle text-warning"></i>
+                  Unsubscribe
+                </div>
+                <hr></hr>
+                <div className="stats">
+                  <i className="far fa-clock"></i>
+                  Campaign sent 2 days ago
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
         </Row>
         <Row>
           <Col md="6">
@@ -185,7 +242,77 @@ function Dashboard() {
                 <p className="card-category">All products including Taxes</p>
               </Card.Header>
               <Card.Body>
-                <div className="ct-chart" id="chartActivity"></div>
+                <div className="ct-chart" id="chartActivity">
+                  <ChartistGraph
+                    data={{
+                      labels: [
+                        "Jan",
+                        "Feb",
+                        "Mar",
+                        "Apr",
+                        "Mai",
+                        "Jun",
+                        "Jul",
+                        "Aug",
+                        "Sep",
+                        "Oct",
+                        "Nov",
+                        "Dec",
+                      ],
+                      series: [
+                        [
+                          542,
+                          443,
+                          320,
+                          780,
+                          553,
+                          453,
+                          326,
+                          434,
+                          568,
+                          610,
+                          756,
+                          895,
+                        ],
+                        [
+                          412,
+                          243,
+                          280,
+                          580,
+                          453,
+                          353,
+                          300,
+                          364,
+                          368,
+                          410,
+                          636,
+                          695,
+                        ],
+                      ],
+                    }}
+                    type="Bar"
+                    options={{
+                      seriesBarDistance: 10,
+                      axisX: {
+                        showGrid: false,
+                      },
+                      height: "245px",
+                    }}
+                    responsiveOptions={[
+                      [
+                        "screen and (max-width: 640px)",
+                        {
+                          seriesBarDistance: 5,
+                          axisX: {
+                            labelInterpolationFnc: function (value) {
+                              return value[0];
+                            },
+                          },
+                        },
+                      ],
+                    ]}
+                  />
+                </div>
               </Card.Body>
               <Card.Footer>
                 <div className="legend">
