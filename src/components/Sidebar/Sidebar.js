da2bc06a5fc8badd1516be7522a22ff0f1ef2d32
EINazare <emanuelioannazare@gmail.com>
2021-01-20 17:39:59 +0200

    fix: add dynamic code for layout

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -16,96 +16,70 @@
 
 */
 import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
+import { useLocation, NavLink } from "react-router-dom";
 
 import { Nav } from "react-bootstrap";
 
 import logo from "assets/img/reactlogo.png";
 
-class Sidebar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      width: window.innerWidth,
-    };
-  }
-  activeRoute(routeName) {
-    return this.props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
-  }
-  updateDimensions() {
-    this.setState({ width: window.innerWidth });
-  }
-  componentDidMount() {
-    this.updateDimensions();
-    window.addEventListener("resize", this.updateDimensions.bind(this));
-  }
-  render() {
-    const sidebarBackground = {
-      backgroundImage: "url(" + this.props.image + ")",
-    };
-    return (
-      <div className="sidebar" data-image="../assets/img/sidebar-5.jpg">
-        {/* Tip 1: You can change the color of the sidebar using: data-color="purple | blue | green | orange | red" Tip 2: you can also add an image using data-image tag */}
-        <div className="sidebar-wrapper">
-          <div className="logo">
-            <a className="simple-text" href="http://www.creative-tim.com">
-              Creative Tim
-            </a>
-          </div>
-          <Nav>
-            <Nav.Item className="active">
-              <Nav.Link href="dashboard.html">
-                <i className="nc-icon nc-chart-pie-35"></i>
-                <p>Dashboard</p>
-              </Nav.Link>
-            </Nav.Item>
-            <li>
-              <Nav.Link href="./user.html">
-                <i className="nc-icon nc-circle-09"></i>
-                <p>User Profile</p>
-              </Nav.Link>
-            </li>
-            <li>
-              <Nav.Link href="./table.html">
-                <i className="nc-icon nc-notes"></i>
-                <p>Table List</p>
-              </Nav.Link>
-            </li>
-            <li>
-              <Nav.Link href="./typography.html">
-                <i className="nc-icon nc-paper-2"></i>
-                <p>Typography</p>
-              </Nav.Link>
-            </li>
-            <li>
-              <Nav.Link href="./icons.html">
-                <i className="nc-icon nc-atom"></i>
-                <p>Icons</p>
-              </Nav.Link>
-            </li>
-            <li>
-              <Nav.Link href="./maps.html">
-                <i className="nc-icon nc-pin-3"></i>
-                <p>Maps</p>
-              </Nav.Link>
-            </li>
-            <li>
-              <Nav.Link href="./notifications.html">
-                <i className="nc-icon nc-bell-55"></i>
-                <p>Notifications</p>
-              </Nav.Link>
-            </li>
-            <Nav.Item className="active active-pro">
-              <Nav.Link className="active" href="upgrade.html">
-                <i className="nc-icon nc-alien-33"></i>
-                <p>Upgrade to PRO</p>
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
+function Sidebar({ color, image, routes }) {
+  const location = useLocation();
+  const activeRoute = (routeName) => {
+    return location.pathname.indexOf(routeName) > -1 ? "active" : "";
+  };
+  return (
+    <div className="sidebar" data-image={image} data-color={color}>
+      <div
+        className="sidebar-background"
+        style={{
+          backgroundImage: "url(" + image + ")",
+        }}
+      />
+      <div className="sidebar-wrapper">
+        <div className="logo d-flex align-items-center justify-content-start">
+          <a
+            href="https://www.creative-tim.com?ref=lbd-sidebar"
+            class="simple-text logo-mini mx-1"
+          >
+            <div class="logo-img">
+              <img
+                src={require("assets/img/reactlogo.png").default}
+                alt="..."
+              />
+            </div>
+          </a>
+          <a className="simple-text" href="http://www.creative-tim.com">
+            Creative Tim
+          </a>
         </div>
+        <Nav>
+          {routes.map((prop, key) => {
+            if (!prop.redirect)
+              return (
+                <li
+                  className={
+                    prop.upgrade
+                      ? "active active-pro"
+                      : activeRoute(prop.layout + prop.path)
+                  }
+                  key={key}
+                >
+                  <NavLink
+                    to={prop.layout + prop.path}
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    <i className={prop.icon} />
+                    <p>{prop.name}</p>
+                  </NavLink>
+                </li>
+              );
+            return null;
+          })}
+        </Nav>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Sidebar;
