844b3757fb64200ac809ec56dee8660eba66a4ec
EINazare <emanuelioannazare@gmail.com>
2021-01-20 13:05:16 +0200

    refactor: change style imports

--- src/index.js
+++ src/index.js
@@ -22,9 +22,8 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/animate.min.css";
-import "./assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0";
+import "./assets/scss/light-bootstrap-dashboard-react.scss?v=1.3.0";
 import "./assets/css/demo.css";
-import "./assets/css/pe-icon-7-stroke.css";
 
 import AdminLayout from "layouts/Admin.jsx";
 
