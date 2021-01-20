4aa5429d150398fae2d2b82d696b1e7b533f0efb
EINazare <emanuelioannazare@gmail.com>
2021-01-20 13:06:29 +0200

    refactor: add new react-bootstrap code

--- src/views/Upgrade.jsx
+++ src/views/Upgrade.jsx
@@ -1,124 +1,128 @@
-/*!
+import React from "react";
 
-=========================================================
-* Light Bootstrap Dashboard React - v1.3.0
-=========================================================
+// react-bootstrap components
+import {
+  Badge,
+  Button,
+  Card,
+  Navbar,
+  Nav,
+  Table,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
 
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React, { Component } from "react";
-import { Table, Grid, Row, Col } from "react-bootstrap";
-
-import Card from "components/Card/Card";
-
-import Button from "components/CustomButton/CustomButton";
-
-class Icons extends Component {
-  render() {
-    return (
-      <div className="content">
-        <Grid fluid>
-          <Row>
-            <Col md={8} mdOffset={2}>
-              <Card
-                hCenter
-                title="Light Bootstrap Dashboard PRO React"
-                category="Are you looking for more components? Please check our Premium Version of Light Bootstrap Dashboard React."
-                ctTableResponsive
-                ctTableFullWidth
-                ctTableUpgrade
-                content={
-                  <Table>
-                    <thead>
-                      <tr>
-                        <th />
-                        <th className="text-center">Free</th>
-                        <th className="text-center">PRO</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>Components</td>
-                        <td>30</td>
-                        <td>60</td>
-                      </tr>
-                      <tr>
-                        <td>Plugins</td>
-                        <td>3</td>
-                        <td>13</td>
-                      </tr>
-                      <tr>
-                        <td>Example Pages</td>
-                        <td>7</td>
-                        <td>24</td>
-                      </tr>
-                      <tr>
-                        <td>Login/Register/Lock Pages</td>
-                        <td>
-                          <i className="fa fa-times text-danger" />
-                        </td>
-                        <td>
-                          <i className="fa fa-check text-success" />
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>Premium Support</td>
-                        <td>
-                          <i className="fa fa-times text-danger" />
-                        </td>
-                        <td>
-                          <i className="fa fa-check text-success" />
-                        </td>
-                      </tr>
-                      <tr>
-                        <td />
-                        <td>Free</td>
-                        <td>Just $49</td>
-                      </tr>
-                      <tr>
-                        <td />
-                        <td>
-                          <Button
-                            href="#"
-                            round
-                            fill
-                            disabled
-                            bsStyle="default"
-                          >
-                            Current Version
-                          </Button>
-                        </td>
-                        <td>
-                          <Button
-                            target="_blank"
-                            href="http://www.creative-tim.com/product/light-bootstrap-dashboard-pro-react/?ref=lbdr-upgrade-page"
-                            round
-                            fill
-                            bsStyle="info"
-                          >
-                            Upgrade to PRO
-                          </Button>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </Table>
-                }
-              />
-            </Col>
-          </Row>
-        </Grid>
-      </div>
-    );
-  }
+function Upgrade() {
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col className=" ml-auto mr-auto" md="8">
+            <Card>
+              <div className=" header text-center">
+                <h4 className=" title">Light Bootstrap Dashboard PRO</h4>
+                <p className=" category">
+                  Are you looking for more components? Please check our Premium
+                  Version of Light Bootstrap Dashboard.
+                </p>
+                <br></br>
+              </div>
+              <Table responsive>
+                <thead>
+                  <th></th>
+                  <th className=" text-center">Free</th>
+                  <th className=" text-center">PRO</th>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Components</td>
+                    <td>16</td>
+                    <td>115+</td>
+                  </tr>
+                  <tr>
+                    <td>Plugins</td>
+                    <td>4</td>
+                    <td>14+</td>
+                  </tr>
+                  <tr>
+                    <td>Example Pages</td>
+                    <td>4</td>
+                    <td>22+</td>
+                  </tr>
+                  <tr>
+                    <td>Documentation</td>
+                    <td>
+                      <i className=" fa fa-times text-danger"></i>
+                    </td>
+                    <td>
+                      <i className=" fa fa-check text-success"></i>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>SASS Files</td>
+                    <td>
+                      <i className=" fa fa-times text-danger"></i>
+                    </td>
+                    <td>
+                      <i className=" fa fa-check text-success"></i>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Login/Register/Lock Pages</td>
+                    <td>
+                      <i className=" fa fa-times text-danger"></i>
+                    </td>
+                    <td>
+                      <i className=" fa fa-check text-success"></i>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>Premium Support</td>
+                    <td>
+                      <i className=" fa fa-times text-danger"></i>
+                    </td>
+                    <td>
+                      <i className=" fa fa-check text-success"></i>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                    <td>Free</td>
+                    <td>Just $39</td>
+                  </tr>
+                  <tr className=" last-row">
+                    <td></td>
+                    <td>
+                      <Button
+                        className=" btn-round btn-fill disabled"
+                        href="#pablo"
+                        onClick={(e) => e.preventDefault()}
+                        variant="default"
+                      >
+                        Current Version
+                      </Button>
+                    </td>
+                    <td>
+                      <Button
+                        className=" btn-round btn-fill"
+                        href="http://www.creative-tim.com/product/light-bootstrap-dashboard-pro/?ref=lbdupgrade"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        variant="info"
+                      >
+                        Upgrade to PRO
+                      </Button>
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
-export default Icons;
+export default Upgrade;
