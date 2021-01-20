d3c8537ca2fddad87fae278649d8bfaf6a93098f
EINazare <emanuelioannazare@gmail.com>
2021-01-20 13:23:21 +0200

    feature: add font awesome import of css

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ import ReactDOM from "react-dom";
 
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/animate.min.css";
 import "./assets/scss/light-bootstrap-dashboard-react.scss?v=1.3.0";
