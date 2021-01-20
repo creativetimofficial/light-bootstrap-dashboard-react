24ee15775ca1b8478cddd6e15771bd2f03c96755
EINazare <emanuelioannazare@gmail.com>
2021-01-20 15:36:10 +0200

    style: run prettier

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -44,60 +44,60 @@ class Sidebar extends Component {
       backgroundImage: "url(" + this.props.image + ")",
     };
     return (
-      <div className=" sidebar" data-image="../assets/img/sidebar-5.jpg">
+      <div className="sidebar" data-image="../assets/img/sidebar-5.jpg">
         {/* Tip 1: You can change the color of the sidebar using: data-color="purple | blue | green | orange | red" Tip 2: you can also add an image using data-image tag */}
-        <div className=" sidebar-wrapper">
-          <div className=" logo">
-            <a className=" simple-text" href="http://www.creative-tim.com">
+        <div className="sidebar-wrapper">
+          <div className="logo">
+            <a className="simple-text" href="http://www.creative-tim.com">
               Creative Tim
             </a>
           </div>
           <Nav>
-            <Nav.Item className=" active">
+            <Nav.Item className="active">
               <Nav.Link href="dashboard.html">
-                <i className=" nc-icon nc-chart-pie-35"></i>
+                <i className="nc-icon nc-chart-pie-35"></i>
                 <p>Dashboard</p>
               </Nav.Link>
             </Nav.Item>
             <li>
               <Nav.Link href="./user.html">
-                <i className=" nc-icon nc-circle-09"></i>
+                <i className="nc-icon nc-circle-09"></i>
                 <p>User Profile</p>
               </Nav.Link>
             </li>
             <li>
               <Nav.Link href="./table.html">
-                <i className=" nc-icon nc-notes"></i>
+                <i className="nc-icon nc-notes"></i>
                 <p>Table List</p>
               </Nav.Link>
             </li>
             <li>
               <Nav.Link href="./typography.html">
-                <i className=" nc-icon nc-paper-2"></i>
+                <i className="nc-icon nc-paper-2"></i>
                 <p>Typography</p>
               </Nav.Link>
             </li>
             <li>
               <Nav.Link href="./icons.html">
-                <i className=" nc-icon nc-atom"></i>
+                <i className="nc-icon nc-atom"></i>
                 <p>Icons</p>
               </Nav.Link>
             </li>
             <li>
               <Nav.Link href="./maps.html">
-                <i className=" nc-icon nc-pin-3"></i>
+                <i className="nc-icon nc-pin-3"></i>
                 <p>Maps</p>
               </Nav.Link>
             </li>
             <li>
               <Nav.Link href="./notifications.html">
-                <i className=" nc-icon nc-bell-55"></i>
+                <i className="nc-icon nc-bell-55"></i>
                 <p>Notifications</p>
               </Nav.Link>
             </li>
-            <Nav.Item className=" active active-pro">
-              <Nav.Link className=" active" href="upgrade.html">
-                <i className=" nc-icon nc-alien-33"></i>
+            <Nav.Item className="active active-pro">
+              <Nav.Link className="active" href="upgrade.html">
+                <i className="nc-icon nc-alien-33"></i>
                 <p>Upgrade to PRO</p>
               </Nav.Link>
             </Nav.Item>
