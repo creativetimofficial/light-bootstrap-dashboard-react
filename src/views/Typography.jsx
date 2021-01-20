524d913fb9c26f1221e55c5de508e57d2fdae0e4
EINazare <emanuelioannazare@gmail.com>
2021-01-20 15:36:25 +0200

    style: run prettier

--- src/views/Typography.jsx
+++ src/views/Typography.jsx
@@ -21,48 +21,48 @@ function Typography() {
             <Card>
               <Card.Header>
                 <Card.Title as="h4">Light Bootstrap Table Heading</Card.Title>
-                <p className=" card-category">
+                <p className="card-category">
                   Created using Montserrat Font Family
                 </p>
               </Card.Header>
               <Card.Body>
-                <div className=" typography-line">
+                <div className="typography-line">
                   <h1>
                     <span>Header 1</span>
                     The Life of LB Dashboard
                   </h1>
                 </div>
-                <div className=" typography-line">
+                <div className="typography-line">
                   <h2>
                     <span>Header 2</span>
                     The Life of Light Bootstrap Dashboard
                   </h2>
                 </div>
-                <div className=" typography-line">
+                <div className="typography-line">
                   <h3>
                     <span>Header 3</span>
                     The Life of Light Bootstrap Dashboard
                   </h3>
                 </div>
-                <div className=" typography-line">
+                <div className="typography-line">
                   <h4>
                     <span>Header 4</span>
                     The Life of Light Bootstrap Dashboard
                   </h4>
                 </div>
-                <div className=" typography-line">
+                <div className="typography-line">
                   <h5>
                     <span>Header 5</span>
                     The Life of Light Bootstrap Dashboard
                   </h5>
                 </div>
-                <div className=" typography-line">
+                <div className="typography-line">
                   <h6>
                     <span>Header 6</span>
                     The Life of Light Bootstrap Dashboard
                   </h6>
                 </div>
-                <div className=" typography-line">
+                <div className="typography-line">
                   <p>
                     <span>Paragraph</span>I will be the leader of a company that
                     ends up being worth billions of dollars, because I got the
@@ -71,10 +71,10 @@ function Typography() {
                     to show people, this is the level that things could be at.
                   </p>
                 </div>
-                <div className=" typography-line">
+                <div className="typography-line">
                   <span>Quote</span>
                   <blockquote>
-                    <p className=" blockquote blockquote-primary">
+                    <p className="blockquote blockquote-primary">
                       "I will be the leader of a company that ends up being
                       worth billions of dollars, because I got the answers. I
                       understand culture. I am the nucleus. I think that’s a
@@ -86,49 +86,49 @@ function Typography() {
                     </p>
                   </blockquote>
                 </div>
-                <div className=" typography-line">
+                <div className="typography-line">
                   <span>Muted Text</span>
-                  <p className=" text-muted">
+                  <p className="text-muted">
                     I will be the leader of a company that ends up being worth
                     billions of dollars, because I got the answers...
                   </p>
                 </div>
-                <div className=" typography-line">
+                <div className="typography-line">
                   <span>Primary Text</span>
-                  <p className=" text-primary">
+                  <p className="text-primary">
                     I will be the leader of a company that ends up being worth
                     billions of dollars, because I got the answers...
                   </p>
                 </div>
-                <div className=" typography-line">
+                <div className="typography-line">
                   <span>Info Text</span>
-                  <p className=" text-info">
+                  <p className="text-info">
                     I will be the leader of a company that ends up being worth
                     billions of dollars, because I got the answers...
                   </p>
                 </div>
-                <div className=" typography-line">
+                <div className="typography-line">
                   <span>Success Text</span>
-                  <p className=" text-success">
+                  <p className="text-success">
                     I will be the leader of a company that ends up being worth
                     billions of dollars, because I got the answers...
                   </p>
                 </div>
-                <div className=" typography-line">
+                <div className="typography-line">
                   <span>Warning Text</span>
-                  <p className=" text-warning">
+                  <p className="text-warning">
                     I will be the leader of a company that ends up being worth
                     billions of dollars, because I got the answers...
                   </p>
                 </div>
-                <div className=" typography-line">
+                <div className="typography-line">
                   <span>Danger Text</span>
-                  <p className=" text-danger">
+                  <p className="text-danger">
                     I will be the leader of a company that ends up being worth
                     billions of dollars, because I got the answers...
                   </p>
                 </div>
-                <div className=" typography-line">
+                <div className="typography-line">
                   <h2>
                     <span>Small Tag</span>
                     Header with small subtitle <br></br>
