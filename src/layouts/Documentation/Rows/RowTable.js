3024117941b64109f74b211cf6e87fda34cd0862
EINazare <emanuelioannazare@gmail.com>
2021-01-21 17:33:42 +0200

    style: run prettier

--- src/layouts/Documentation/Rows/RowTable.js
+++ src/layouts/Documentation/Rows/RowTable.js
@@ -20,14 +20,14 @@ import { Table } from "react-bootstrap";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { prism } from "react-syntax-highlighter/dist/styles/prism";
 
-const thArray = ["ID","Name","Salary","Country","City"];
+const thArray = ["ID", "Name", "Salary", "Country", "City"];
 const tdArray = [
-    [ "1" , "Dakota Rice" , "$36,738" , "Niger" , "Oud-Turnhout" ] ,
-    [ "2" , "Minerva Hooper" , "$23,789" , "Curaçao" , "Sinaai-Waas" ] ,
-    [ "3" , "Sage Rodriguez" , "$56,142" , "Netherlands" , "Baileux" ] ,
-    [ "4" , "Philip Chaney" , "$38,735" , "Korea, South" , "Overland Park" ] ,
-    [ "5" , "Doris Greene" , "$63,542" , "Malawi" , "Feldkirchen in Kärnten" ] ,
-    [ "6" , "Mason Porter" , "$78,615" , "Chile" , "Gloucester" ]
+  ["1", "Dakota Rice", "$36,738", "Niger", "Oud-Turnhout"],
+  ["2", "Minerva Hooper", "$23,789", "Curaçao", "Sinaai-Waas"],
+  ["3", "Sage Rodriguez", "$56,142", "Netherlands", "Baileux"],
+  ["4", "Philip Chaney", "$38,735", "Korea, South", "Overland Park"],
+  ["5", "Doris Greene", "$63,542", "Malawi", "Feldkirchen in Kärnten"],
+  ["6", "Mason Porter", "$78,615", "Chile", "Gloucester"],
 ];
 
 const codeExamplethArray = `const thArray = ["ID","Name","Salary","Country","City"];`;
