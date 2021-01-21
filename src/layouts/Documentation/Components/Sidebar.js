5a9b0f5a4f9d0ae3e52405dfc8877965194de22b
EINazare <emanuelioannazare@gmail.com>
2021-01-21 11:37:14 +0200

    refactor: change jsx to js

---
+++ src/layouts/Documentation/Components/Sidebar.js
@@ -0,0 +1,57 @@
+/*!
+
+=========================================================
+* Light Bootstrap Dashboard React - v1.3.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React, { Component } from "react";
+import { Affix } from "react-overlays";
+import { Link } from "react-router-dom";
+
+import documentationRoutes from "documentation.js";
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <Affixed>
+        <ul className="nav nav-pills nav-stacked fixed-section">
+          {documentationRoutes.map((prop, key) => {
+            if (prop.redirect) {
+              return null;
+            }
+            return (
+              <li key={key}>
+                <Link to={prop.path} className="nav-link">
+                  {prop.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </Affixed>
+    );
+  }
+}
+
+class Affixed extends Component {
+  render() {
+    return window.innerWidth > 991 ? (
+      <Affix offsetTop={400}>{this.props.children}</Affix>
+    ) : (
+      this.props.children
+    );
+  }
+}
+
+export default Sidebar;
