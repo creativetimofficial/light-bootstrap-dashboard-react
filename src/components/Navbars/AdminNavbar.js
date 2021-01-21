b46c0138d01f878c68a7564b8d190267868a2859
EINazare <emanuelioannazare@gmail.com>
2021-01-21 10:46:19 +0200

    refactor: add dynamic brand text

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -16,15 +16,14 @@
 
 */
 import React, { Component } from "react";
+import { useLocation } from "react-router-dom";
 import { Navbar, Container, Nav, Dropdown, Button } from "react-bootstrap";
 
+import routes from "routes.js";
+
 function Header() {
+  const location = useLocation();
   const mobileSidebarToggle = (e) => {
-    // if (this.state.sidebarExists === false) {
-    //   this.setState({
-    //     sidebarExists: true,
-    //   });
-    // }
     e.preventDefault();
     document.documentElement.classList.toggle("nav-open");
     var node = document.createElement("div");
@@ -34,6 +33,15 @@ function Header() {
       document.documentElement.classList.toggle("nav-open");
     };
     document.body.appendChild(node);
+  };
+
+  const getBrandText = () => {
+    for (let i = 0; i < routes.length; i++) {
+      if (location.pathname.indexOf(routes[i].layout + routes[i].path) !== -1) {
+        return routes[i].name;
+      }
+    }
+    return "Brand";
   };
   return (
     <Navbar bg="light" expand="lg">
@@ -51,7 +59,7 @@ function Header() {
             onClick={(e) => e.preventDefault()}
             className="mr-2"
           >
-            Dashboard
+            {getBrandText()}
           </Navbar.Brand>
         </div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-2">
