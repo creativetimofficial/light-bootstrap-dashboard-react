1321fcf0267913dc58ec09a0be6af9fff01e9f84
EINazare <emanuelioannazare@gmail.com>
2021-01-21 17:33:36 +0200

    refactor: add tooltips docs

--- src/layouts/Documentation/Rows/RowTooltip.js
+++ src/layouts/Documentation/Rows/RowTooltip.js
@@ -16,23 +16,18 @@
 
 */
 import React, { Component } from "react";
-import { Tooltip, OverlayTrigger } from "react-bootstrap";
+import { Tooltip, OverlayTrigger, Button } from "react-bootstrap";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { prism } from "react-syntax-highlighter/dist/styles/prism";
 
-import Button from "components/CustomButton/CustomButton.js";
-
-const edit = <Tooltip id="edit_tooltip">Edit Task</Tooltip>;
-const remove = <Tooltip id="remove_tooltip">Remove</Tooltip>;
-
-const codeEdit = `const edit = (<Tooltip id="edit_tooltip">Edit Task</Tooltip>);`;
-const codeRemove = `const remove = (<Tooltip id="remove_tooltip">Remove</Tooltip>);`;
-const codeExmaple = `<OverlayTrigger placement="top" overlay={edit}>
+const codeImport = `import { Tooltip, OverlayTrigger, Button } from 'react-bootstrap';`;
+const codeExampleTooltips = `const edit = (<Tooltip id="edit_tooltip">Edit Task</Tooltip>);
+const remove = (<Tooltip id="remove_tooltip">Remove</Tooltip>);`;
+const codeExample = `<OverlayTrigger placement="top" overlay={edit}>
     <Button
-        bsStyle="info"
-        simple
+        variant="info"
         type="button"
-        bsSize="xs"
+        size="xs"
     >
         <i className="fa fa-edit"></i>
     </Button>
@@ -40,68 +35,56 @@ const codeExmaple = `<OverlayTrigger placement="top" overlay={edit}>
 
 <OverlayTrigger placement="top" overlay={remove}>
     <Button
-        bsStyle="danger"
-        simple
+        variant="danger"
         type="button"
-        bsSize="xs"
+        size="xs"
     >
         <i className="fa fa-times"></i>
     </Button>
 </OverlayTrigger>`;
-
-class RowTooltip extends Component {
+class TooltipSection extends Component {
   render() {
+    const edit = <Tooltip id="edit_tooltip">Edit Task</Tooltip>;
+    const remove = <Tooltip id="remove_tooltip">Remove</Tooltip>;
     return (
-      <div id="tooltip-row" className="tim-row">
-        <h2>Tooltip</h2>
-        <legend />
-        <p>
-          To use this component you need to import it from react-bootstrap like
-          this:
-        </p>
-        <SyntaxHighlighter language="jsx" style={prism}>
-          {`import {Tooltip,OverlayTrigger} from 'react-bootstrap';`}
-        </SyntaxHighlighter>
+      <div className="tim-container">
+        <div className="tim-row">
+          <h2>Tooltips</h2>
+          <legend />
+          <p>
+            You will see the usage of these components in the <code>Table</code>{" "}
+            section.
+          </p>
+          <h4>Imports</h4>
+          <p>For this component you'll need the following imports:</p>
+          <SyntaxHighlighter language="jsx" style={prism}>
+            {codeImport}
+          </SyntaxHighlighter>
+          <h4>Example</h4>
+          <OverlayTrigger placement="top" overlay={edit}>
+            <Button variant="info" simple type="button" size="xs">
+              <i className="fa fa-edit" />
+            </Button>
+          </OverlayTrigger>
 
-        <h4>Example code</h4>
-        <OverlayTrigger placement="top" overlay={edit}>
-          <Button bsStyle="info" simple type="button" bsSize="xs">
-            <i className="fa fa-edit" />
-          </Button>
-        </OverlayTrigger>
-
-        <OverlayTrigger placement="top" overlay={remove}>
-          <Button bsStyle="danger" simple type="button" bsSize="xs">
-            <i className="fa fa-times" />
-          </Button>
-        </OverlayTrigger>
-        <br />
-        <SyntaxHighlighter language="jsx" style={prism}>
-          {codeEdit}
-        </SyntaxHighlighter>
-        <SyntaxHighlighter language="jsx" style={prism}>
-          {codeRemove}
-        </SyntaxHighlighter>
-        <SyntaxHighlighter language="jsx" style={prism}>
-          {codeExmaple}
-        </SyntaxHighlighter>
-        <h4>Details</h4>
-        <p>
-          In this examples we have used <code>Button</code> elements for the
-          tooltips to appear over. You can use different elements. For more
-          information about tooltips and how to use them, please refer to{" "}
-          <a
-            href="https://react-bootstrap.github.io/components/tooltips/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            react-bootstrap documentation
-          </a>
-          .
-        </p>
+          <OverlayTrigger placement="top" overlay={remove}>
+            <Button variant="danger" simple type="button" size="xs">
+              <i className="fa fa-times" />
+            </Button>
+          </OverlayTrigger>
+          <SyntaxHighlighter language="jsx" style={prism}>
+            {codeExample}
+          </SyntaxHighlighter>
+          <p>
+            And the definition of the <code>Tooltip</code>s:
+          </p>
+          <SyntaxHighlighter language="jsx" style={prism}>
+            {codeExampleTooltips}
+          </SyntaxHighlighter>
+        </div>
       </div>
     );
   }
 }
 
-export default RowTooltip;
+export default TooltipSection;
