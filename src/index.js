05b587311cdecd519e7c36895f70e6bc5690aab2
EINazare <emanuelioannazare@gmail.com>
2021-01-21 14:18:06 +0200

    fix: add docs css

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/animate.min.css";
 import "./assets/scss/light-bootstrap-dashboard-react.scss?v=2.0.0";
 import "./assets/css/demo.css";
+import "layouts/Documentation/assets/css/documentation.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import AdminLayout from "layouts/Admin.js";
