e8367bc39c676fde013f4e95115747e60d8e9a19
EINazare <emanuelioannazare@gmail.com>
2021-01-21 11:15:15 +0200

    fix: solve body click issues

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -57,6 +57,8 @@ function Admin() {
       document.documentElement.className.indexOf("nav-open") !== -1
     ) {
       document.documentElement.classList.toggle("nav-open");
+      var element = document.getElementById("bodyClick");
+      element.parentNode.removeChild(element);
     }
   }, [location]);
   return (
