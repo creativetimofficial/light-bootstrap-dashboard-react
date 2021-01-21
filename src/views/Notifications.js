3ac876ac5b292dad38ba8783f59ff250698ff355
EINazare <emanuelioannazare@gmail.com>
2021-01-21 10:30:45 +0200

    style: run prettier

--- src/views/Notifications.js
+++ src/views/Notifications.js
@@ -16,47 +16,47 @@ import {
 } from "react-bootstrap";
 
 function Notifications() {
-  const [showModal,setShowModal] = React.useState(false);
+  const [showModal, setShowModal] = React.useState(false);
   const notificationAlertRef = React.useRef(null);
-const notify = (place) => {
-  var color = Math.floor(Math.random() * 5 + 1);
-  var type;
-  switch (color) {
-    case 1:
-      type = "primary";
-      break;
-    case 2:
-      type = "success";
-      break;
-    case 3:
-      type = "danger";
-      break;
-    case 4:
-      type = "warning";
-      break;
-    case 5:
-      type = "info";
-      break;
-    default:
-      break;
-  }
-  var options = {};
-  options = {
-    place: place,
-    message: (
-      <div>
+  const notify = (place) => {
+    var color = Math.floor(Math.random() * 5 + 1);
+    var type;
+    switch (color) {
+      case 1:
+        type = "primary";
+        break;
+      case 2:
+        type = "success";
+        break;
+      case 3:
+        type = "danger";
+        break;
+      case 4:
+        type = "warning";
+        break;
+      case 5:
+        type = "info";
+        break;
+      default:
+        break;
+    }
+    var options = {};
+    options = {
+      place: place,
+      message: (
         <div>
-          Welcome to <b>Light Bootstrap Dashboard React</b> - a beautiful freebie for
-          every web developer.
+          <div>
+            Welcome to <b>Light Bootstrap Dashboard React</b> - a beautiful
+            freebie for every web developer.
+          </div>
         </div>
-      </div>
-    ),
-    type: type,
-    icon: "nc-icon nc-bell-55",
-    autoDismiss: 7,
+      ),
+      type: type,
+      icon: "nc-icon nc-bell-55",
+      autoDismiss: 7,
+    };
+    notificationAlertRef.current.notificationAlert(options);
   };
-  notificationAlertRef.current.notificationAlert(options);
-};
   return (
     <>
       <div className="rna-container">
@@ -114,7 +114,10 @@ const notify = (place) => {
                   >
                     <i className="nc-icon nc-simple-remove"></i>
                   </button>
-                  <span data-notify="icon" className="nc-icon nc-bell-55"></span>
+                  <span
+                    data-notify="icon"
+                    className="nc-icon nc-bell-55"
+                  ></span>
                   <span>
                     This is a notification with close button and icon.
                   </span>
@@ -128,7 +131,10 @@ const notify = (place) => {
                   >
                     <i className="nc-icon nc-simple-remove"></i>
                   </button>
-                  <span data-notify="icon" className="nc-icon nc-bell-55"></span>
+                  <span
+                    data-notify="icon"
+                    className="nc-icon nc-bell-55"
+                  ></span>
                   <span>
                     This is a notification with close button and icon and have
                     many lines. You can see that the icon and the close button
@@ -226,58 +232,34 @@ const notify = (place) => {
               </Row>
               <Row className="justify-content-center">
                 <Col lg="3" md="3">
-                  <Button
-                    block
-                    onClick={() => notify("tl")}
-                    variant="default"
-                  >
+                  <Button block onClick={() => notify("tl")} variant="default">
                     Top Left
                   </Button>
                 </Col>
                 <Col lg="3" md="3">
-                  <Button
-                    block
-                    onClick={() => notify("tc")}
-                    variant="default"
-                  >
+                  <Button block onClick={() => notify("tc")} variant="default">
                     Top Center
                   </Button>
                 </Col>
                 <Col lg="3" md="3">
-                  <Button
-                    block
-                    onClick={() => notify("tr")}
-                    variant="default"
-                  >
+                  <Button block onClick={() => notify("tr")} variant="default">
                     Top Right
                   </Button>
                 </Col>
               </Row>
               <Row className="justify-content-center">
                 <Col lg="3" md="3">
-                  <Button
-                    block
-                    onClick={() => notify("bl")}
-                    variant="default"
-                  >
+                  <Button block onClick={() => notify("bl")} variant="default">
                     Bottom Left
                   </Button>
                 </Col>
                 <Col lg="3" md="3">
-                  <Button
-                    block
-                    onClick={() => notify("bc")}
-                    variant="default"
-                  >
+                  <Button block onClick={() => notify("bc")} variant="default">
                     Bottom Center
                   </Button>
                 </Col>
                 <Col lg="3" md="3">
-                  <Button
-                    block
-                    onClick={() => notify("br")}
-                    variant="default"
-                  >
+                  <Button block onClick={() => notify("br")} variant="default">
                     Bottom Right
                   </Button>
                 </Col>
@@ -298,7 +280,11 @@ const notify = (place) => {
           </Card.Body>
         </Card>
         {/* Mini Modal */}
-        <Modal className="modal-mini modal-primary" show={showModal} onHide={() => setShowModal(false)}>
+        <Modal
+          className="modal-mini modal-primary"
+          show={showModal}
+          onHide={() => setShowModal(false)}
+        >
           <Modal.Header className="justify-content-center">
             <div className="modal-profile">
               <i className="nc-icon nc-bulb-63"></i>
@@ -308,14 +294,19 @@ const notify = (place) => {
             <p>Always have an access to your profile</p>
           </Modal.Body>
           <div className="modal-footer">
-            <Button className="btn-simple" type="button" variant="link"  onClick={() => setShowModal(false)}>
+            <Button
+              className="btn-simple"
+              type="button"
+              variant="link"
+              onClick={() => setShowModal(false)}
+            >
               Back
             </Button>
             <Button
               className="btn-simple"
               type="button"
               variant="link"
-             onClick={() => setShowModal(false)}
+              onClick={() => setShowModal(false)}
             >
               Close
             </Button>
