f7b1f02c6d73299e180b6e4fc96322059666e8c6
EINazare <emanuelioannazare@gmail.com>
2021-01-21 17:30:26 +0200

    refactor: add radio docs

--- src/layouts/Documentation/Rows/RowRadio.js
+++ src/layouts/Documentation/Rows/RowRadio.js
@@ -16,167 +16,71 @@
 
 */
 import React, { Component } from "react";
-import { Table } from "react-bootstrap";
-import Radio from "components/CustomRadio/CustomRadio";
+import { Form } from "react-bootstrap";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { prism } from "react-syntax-highlighter/dist/styles/prism";
 
-const codeExample = `<Radio
-    number="3"
-    option="1"
-    name="radio"
-    onChange={this.handleRadio}
-    checked={this.state.radio === "1"}
-    label="Checked"
-/>
-<Radio
-    number="4"
-    option="2"
-    name="radio"
-    onChange={this.handleRadio}
-    checked={this.state.radio === "2"}
-    label="Unchecked"
-/>`;
-const codeExampleDependecies = `constructor(props) {
-    super(props);
-    this.state = {
-        radio: "1"
-    };
-}
+const codeColors = `<div>
+  <Form.Check className="mb-1 pl-0">
+    <Form.Check.Label>
+      <Form.Check.Input
+        defaultChecked
+        type="radio" name="radio-1"
+      ></Form.Check.Input>
+      <span className="form-check-sign"></span>
+      Checked
+    </Form.Check.Label>
+  </Form.Check>
+  <Form.Check className="mb-1 pl-0">
+    <Form.Check.Label>
+      <Form.Check.Input type="radio" name="radio-1"></Form.Check.Input>
+      <span className="form-check-sign"></span>
+      Unchecked
+    </Form.Check.Label>
+  </Form.Check>
+</div>`;
 
-handleRadio = event => {
-    const target = event.target;
-    this.setState({
-        [target.name]: target.value
-    });
-};`;
-
-class RowCheckbox extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      radio: "1",
-    };
-  }
-
-  handleRadio = (event) => {
-    const target = event.target;
-    this.setState({
-      [target.name]: target.value,
-    });
-  };
+class RowRadio extends Component {
   render() {
     return (
-      <div className="tim-row" id="radio-row">
-        <h2>RadioButtons</h2>
+      <div className="tim-row" id="checkbox-row">
+        <h2>Checkboxes</h2>
         <legend />
         <p>
-          To use the custom radiobuttons controls you need to import the custom
-          made component:
+          To use the custom checkbox controls you need to import the custom made
+          component:
         </p>
         <SyntaxHighlighter language="jsx" style={prism}>
-          {`import Radio from 'components/CustomRadio/CustomRadio';`}
+          {`import Form from 'react-bootstrap';`}
         </SyntaxHighlighter>
         <div>
-          <Radio
-            number="3"
-            option="1"
-            name="radio"
-            onChange={this.handleRadio}
-            checked={this.state.radio === "1"}
-            label="Checked"
-          />
-          <Radio
-            number="4"
-            option="2"
-            name="radio"
-            onChange={this.handleRadio}
-            checked={this.state.radio === "2"}
-            label="Unchecked"
-          />
+          <Form.Check className="mb-1 pl-0">
+            <Form.Check.Label>
+              <Form.Check.Input
+                defaultChecked
+                type="radio" name="radio-1"
+              ></Form.Check.Input>
+              <span className="form-check-sign"></span>
+              Checked
+            </Form.Check.Label>
+          </Form.Check>
+          <Form.Check className="mb-1 pl-0">
+            <Form.Check.Label>
+              <Form.Check.Input type="radio" name="radio-1"></Form.Check.Input>
+              <span className="form-check-sign"></span>
+              Unchecked
+            </Form.Check.Label>
+          </Form.Check>
         </div>
-
         <SyntaxHighlighter language="jsx" style={prism}>
-          {codeExample}
-        </SyntaxHighlighter>
-
-        <SyntaxHighlighter language="jsx" style={prism}>
-          {codeExampleDependecies}
+          {codeColors}
         </SyntaxHighlighter>
 
         <h4>Props</h4>
-        <Table striped bordered condensed hover>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Type</th>
-              <th>Default</th>
-              <th>Description</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <code>number</code>
-              </td>
-              <td>string</td>
-              <td>null</td>
-              <td>
-                This is the unique id of each checkbox. You need to set this for
-                checkboxes to work properly.
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <code>option</code>
-              </td>
-              <td>string</td>
-              <td>null</td>
-              <td>Use this flag to add obption of the radio.</td>
-            </tr>
-            <tr>
-              <td>
-                <code>name</code>
-              </td>
-              <td>string</td>
-              <td>null</td>
-              <td>Use this flag to add the name of the radio group.</td>
-            </tr>
-            <tr>
-              <td>
-                <code>label</code>
-              </td>
-              <td>string / node</td>
-              <td>null</td>
-              <td>
-                Use this flag to add text or a node as a label to the checkbox.
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <code>onChange</code>
-              </td>
-              <td>function</td>
-              <td>null</td>
-              <td>This is the prop that changes the default checked radio.</td>
-            </tr>
-            <tr>
-              <td>
-                <code>checked</code>
-              </td>
-              <td>boolean</td>
-              <td>true</td>
-              <td>
-                This is the prop to make one radio checked by default. If you
-                set this porp you need to set onChange prop too.
-              </td>
-            </tr>
-          </tbody>
-        </Table>
         <p>
           Beside these props you can also reffer to{" "}
           <a
-            href="https://react-bootstrap.github.io/components/forms/#forms-props-radio"
+            href="https://react-bootstrap.github.io/components/forms/#forms-props-checkbox"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -189,4 +93,4 @@ class RowCheckbox extends Component {
   }
 }
 
-export default RowCheckbox;
+export default RowRadio;
